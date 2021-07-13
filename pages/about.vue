eb92c1c45c022937dbec07ff762d32bc664014fb
jalin <jailin1124@gmail.com>
2021-07-13 17:15:18 +0800

    關於我頁面調整

--- pages/about.vue
+++ pages/about.vue
@@ -9,7 +9,7 @@
                 <h2>關於我</h2>
                 <div class="abount-content">
                     一個網頁程式設計師，擅長 PHP
-                    網頁程式開發，目前在南部一家公司，擔任網頁程式設計師，主要負責前後端程式開發。
+                    網頁程式開發，目前在南部一家接案公司，擔任網頁程式設計師，主要負責前後端程式開發。平時喜歡帶著<s>相機</s>(手機)到處趴趴走，紀錄每座城市的美好。
                 </div>
                 <h2>技能樹</h2>
                 <div class="row row-col-1 row-col-sm-2 row-col-md-4 skill-tree-box">
@@ -70,6 +70,10 @@
                             </div>
                         </div>
                     </div>
+                </div>
+                <h2>關於本站</h2>
+                <div class="abount-content">
+                    關於這個站主要是為了紀錄技術筆記，及一些我覺得好用的軟體工具。本站是由 <code>nuxt.js</code> 所建置而成。
                 </div>
             </div>
         </div>
